9f53ae0a93b2a6a744d34cb1bdca35170eb0c76a
Efremov Egor <efremovegor936@gmail.com>
2024-02-05 02:43:21 +0300

    update 4 files and delete 1 file

--- backend/simple_chat/api/urls.py
+++ backend/simple_chat/api/urls.py
@@ -3,7 +3,6 @@ from django.views.generic import TemplateView
 from api import views
 
 urlpatterns = [
-    path("api/", include("rest_framework.urls", namespace="rest_framework")),
     path("tweets", views.TweetsView.as_view()),
     path("profiles/", views.ProfileList.as_view()),
 ]
